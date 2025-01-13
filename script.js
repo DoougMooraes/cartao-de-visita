4697c3c59e06a922cb20d3af36c811edd7029061
Doougmooraes <dkcgamesstudio@gmail.com>
2025-01-13 13:48:40 -0300

    add readme

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function toggleMode() {
 
     if (html.classList.contains ("light")) {
 
-        img.setAttribute ("src", "./assets/avaight.png"tar-l )
+        img.setAttribute ("src", "./assets/avatar-light.png" )
 
     } else {
 
